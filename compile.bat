f3914d560b3da609c2d1dfb6258d6189c0e65047
duchenpaul <duchenpaul@gmail.com>
2020-06-16 18:55:43 +0800

    update compile script

--- compile.bat
+++ compile.bat
@@ -1,10 +1,10 @@
 SET appName=text2keystroke_gui
-pyinstaller text2keystroke_gui.spec
-rem pyinstaller .\%appName%.py --windowed --hidden-import PyQt5.sip  
+pyinstaller --noconfirm text2keystroke_gui.spec
+rem pyinstaller --noconfirm .\%appName%.py --windowed --hidden-import PyQt5.sip  
 rem move dist\%appName%.exe %cd%
 
 rem del /s /f /q  text2keystroke.spec
 
-rem REM walkaround RD bug
+REM walkaround RD bug
 rem timeout 1
 rem RD /S /Q   dist\ build\
